7c9d18bba7f75e50af27691e069806e0bc6e1441
Vedant <borodevedant4@gmail.com>
2025-06-11 15:13:17 +0530

    feat(database): establish database connection using Mongoose and environment variables

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,9 +2,12 @@
 import 'dotenv/config'
 import express from "express";
 import projectRoutes from './routes/project.route';
+import connectDB from './db/index';
 
 const app = express()
 const PORT = process.env.PORT || 3000
+
+connectDB()
 
 app.use("/api/v1", projectRoutes)
 
