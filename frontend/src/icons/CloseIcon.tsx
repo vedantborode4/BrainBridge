828081854a6652f94aaebbb377bb6e89a20fb15e
Vedant <borodevedant4@gmail.com>
2025-07-13 00:42:31 +0530

    feat(icons): add icon components

--- frontend/src/icons/CloseIcon.tsx
+++ frontend/src/icons/CloseIcon.tsx
@@ -1,9 +1,9 @@
 import { type IconProps, iconSizeVariants } from "."
 
-export const PlusIcon = (props: IconProps) => {
+export const CloseIcon = (props: IconProps) => {
 
     return <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className={iconSizeVariants[props.size]}>
-        <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
     </svg>
-  
+    
 }
