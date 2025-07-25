af15e6354ee1b42850aac2d7b0015abde5c2e7e9
Vedant <borodevedant4@gmail.com>
2025-07-25 23:15:34 +0530

    feat(backend): add cors middleware with env-based frontend origin and update .env.sample

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,11 +3,18 @@ import 'dotenv/config'
 import express from "express";
 import projectRoutes from './routes/project.route';
 import connectDB from './db/index';
+import cors from 'cors'
 
 const app = express()
-const PORT = process.env.PORT || 3000
-
+const PORT = process.env.PORT || 3000 ;
+const FRONTENDURL =  process.env.FRONTENDURL ;
 connectDB()
+
+app.use(cors({
+    origin: FRONTENDURL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+}))
 
 app.use(express.json());
 app.use("/api/v1", projectRoutes)
